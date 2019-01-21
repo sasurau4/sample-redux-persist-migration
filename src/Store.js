da8e2a7901dff0fb075087fbf2f65b2ec8760404
Daiki Ihara <sasurau4@gmail.com>
2019-01-21 21:17:56 +0900

    add v-1 to v0 migration

--- src/Store.js
+++ src/Store.js
@@ -1,12 +1,26 @@
 import { createStore } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
+import { persistStore, persistReducer, createMigrate } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import rootReducer from './reducers';
 
+const migrations = {
+  0: state => {
+    return {
+      ...state,
+      favorite: {
+        ...state.favorite,
+        animal: 'Tiger',
+      },
+    };
+  },
+};
+
 const persistConfig = {
   key: 'root',
+  version: 0,
   storage,
   debug: true,
+  migrate: createMigrate(migrations, { debug: true }),
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
